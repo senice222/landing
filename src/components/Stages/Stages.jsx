626c11e33e14bf1a4c518be09e3bbc50039436da
senice222 <artemprimorksij7@gmail.com>
2024-05-21 21:43:23 +0300

    detailed product

--- src/components/Stages/Stages.jsx
+++ src/components/Stages/Stages.jsx
@@ -16,18 +16,7 @@ const Stages = () => {
                     transition={{ delay: 0.1, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 1 }, ease: "easeIn", duration: 1 }}
                 >{t("STAGES ")} <span>{t("INTERACTION")}</span></motion.h3>
                 <div className={style.itemsContainer}>
-                    <motion.div
-                        initial={{ x: -100, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        transition={{
-                            delay: 0.4,
-                            x: { type: 'spring', stiffness: 60 },
-                            opacity: { duration: 0.2 },
-                            ease: "easeIn",
-                            duration: 1
-                        }}
-                        viewport={{ once: true, amount: 0.5 }} 
-                        className={style.item}>
+                    <div className={style.item}>
                         <motion.div
                             initial={{ x: -100, opacity: 0 }}
                             whileInView={{ x: 0, opacity: 1 }}
@@ -43,21 +32,15 @@ const Stages = () => {
                                 {t("Application")}
                             </motion.p>
                         </motion.div>
-                        <div className={style.descr}>
+                        <motion.div
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
+                            className={style.descr}>
                             {t("After leaving a request we will contact you and offer you to fill in the specification, in which you will have to specify in detail the required goods and their features.")}
-                        </div>
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: -100, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        transition={{
-                            delay: 0.4,
-                            x: { type: 'spring', stiffness: 60 },
-                            opacity: { duration: 0.2 },
-                            ease: "easeIn",
-                            duration: 1
-                        }}
-                        viewport={{ once: true, amount: 0.5 }} 
+                        </motion.div>
+                    </div>
+                    <div
                         className={style.item}>
                         <motion.div
                             initial={{ x: -100, opacity: 0 }}
@@ -74,21 +57,16 @@ const Stages = () => {
                                 {t("Search for goods")}
                             </motion.p>
                         </motion.div>
-                        <div className={style.descr}>
+                        <motion.div
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
+                            className={style.descr}>
                             {t("We will search for manufacturers of the required goods within three days. After that we will contact you and present the terms and conditions offered by them. The first three products found will be found for you free of charge.")}
-                        </div>
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: -100, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        transition={{
-                            delay: 0.4,
-                            x: { type: 'spring', stiffness: 60 },
-                            opacity: { duration: 0.2 },
-                            ease: "easeIn",
-                            duration: 1
-                        }}
-                        viewport={{ once: true, amount: 0.5 }} 
+                        </motion.div>
+                    </div>
+                    <div
+
                         className={style.item}>
                         <motion.div
                             initial={{ x: -100, opacity: 0 }}
@@ -105,21 +83,16 @@ const Stages = () => {
                                 {t("Cost calculation")}
                             </motion.p>
                         </motion.div>
-                        <div className={style.descr}>
+                        <motion.div
+                            initial={{ x: -100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
+                            className={style.descr}>
                             {t("We make calculations on logistics and our commission, specifying at once whether you need additional warehouse services such as barcoding, checking the goods for defects and so on")}
-                        </div>
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: 100, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        transition={{
-                            delay: 0.4,
-                            x: { type: 'spring', stiffness: 60 },
-                            opacity: { duration: 0.2 },
-                            ease: "easeIn",
-                            duration: 1
-                        }}
-                        viewport={{ once: true, amount: 0.5 }} 
+                        </motion.div>
+                    </div>
+                    <div
+
                         className={style.item}>
                         <motion.div
                             initial={{ x: 100, opacity: 0 }}
@@ -136,22 +109,15 @@ const Stages = () => {
                                 {t("Payment for Services")}
                             </motion.p>
                         </motion.div>
-                        <div className={style.descr}>
+                        <motion.div
+                            initial={{ x: 100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
+                            className={style.descr}>
                             {t("If you agree to our co-operation, you pay the cost of goods and the cost of our services.")}
-                        </div>
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: 100, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        transition={{
-                            delay: 0.4,
-                            x: { type: 'spring', stiffness: 60 },
-                            opacity: { duration: 0.2 },
-                            ease: "easeIn",
-                            duration: 1
-                        }}
-                        viewport={{ once: true, amount: 0.5 }} 
-                        className={style.item}>
+                        </motion.div>
+                    </div>
+                    <div className={style.item}>
                         <motion.div
                             initial={{ x: 100, opacity: 0 }}
                             whileInView={{ x: 0, opacity: 1 }}
@@ -167,22 +133,15 @@ const Stages = () => {
                                 {t("Redemption of goods")}
                             </motion.p>
                         </motion.div>
-                        <div className={style.descr}>
+                        <motion.div
+                            initial={{ x: 100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
+                            className={style.descr}>
                             {t("We buy a batch of goods from the manufacturer, after which the factory sends it to our warehouse. We accept the goods, make a record and send you a video review.")}
-                        </div>
-                    </motion.div>
-                    <motion.div
-                        initial={{ x: 100, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        transition={{
-                            delay: 0.4,
-                            x: { type: 'spring', stiffness: 60 },
-                            opacity: { duration: 0.2 },
-                            ease: "easeIn",
-                            duration: 1
-                        }}
-                        viewport={{ once: true, amount: 0.5 }} 
-                        className={style.item}>
+                        </motion.div>
+                    </div>
+                    <div className={style.item}>
                         <motion.div
                             initial={{ x: 100, opacity: 0 }}
                             whileInView={{ x: 0, opacity: 1 }}
@@ -196,12 +155,16 @@ const Stages = () => {
                                 transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
                             >
                                 {t("Checking and dispatch")}
-                                </motion.p>
+                            </motion.p>
                         </motion.div>
-                        <div className={style.descr}>
+                        <motion.div
+                            initial={{ x: 100, opacity: 0 }}
+                            whileInView={{ x: 0, opacity: 1 }}
+                            transition={{ delay: 0.4, x: { type: 'spring', stiffness: 60 }, opacity: { duration: 0.2 }, ease: "easeIn", duration: 1 }}
+                            className={style.descr}>
                             {t("We recalculate the goods, check for quality on request and send them to your country.")}
-                        </div>
-                    </motion.div>
+                        </motion.div>
+                    </div>
                 </div>
             </div>
             <div className={style.line} />
